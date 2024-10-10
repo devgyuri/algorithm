95d2dc8fd5334e5e4917421d698ea123cdb4e036
devgyuri <gksmf910@gmail.com>
2024-10-10 21:55:25 +0900

    tle: nested for loop

---
+++ leetcode/twoPointer/962.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        int maxWidth = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] <= nums[j]) {
+                    maxWidth = Math.max(maxWidth, j - i);
+                }
+            }
+        }
+        return maxWidth;
+    }
+}
