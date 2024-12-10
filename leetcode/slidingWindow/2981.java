362c9bc48042178e1ea31fe9f9720b0bb1e6d2a7
devgyuri <gksmf910@gmail.com>
2024-12-10 18:46:35 +0900

    error: sliding window + binary search

---
+++ leetcode/slidingWindow/2981.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumLength(String s) {
+        int n = s.length();
+        int left = 0;
+        int right = n - 1;
+    }
+
+    private boolean isValidWindow(String s, int w) {
+        int n = s.length();
+        int[] count = new int[26];
+        int start = 0;
+        while (start < n) {
+            
+        }
+    }
+}
