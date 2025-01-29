ed175ec6ebd44111a3bf908c4e6dc71b09ec85f2
devgyuri <gksmf910@gmail.com>
2025-01-29 19:09:41 +0900

    solution: dfs

---
+++ leetcode/graph/684.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        List<Integer>[] adjList = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            boolean[] visited = new boolean[n];
+            if (isConnected(edge[0] - 1, edge[1] - 1, visited, adjList)) {
+                return new int[] {edge[0], edge[1]};
+            }
+            adjList[edge[0] - 1].add(edge[1] - 1);
+            adjList[edge[1] - 1].add(edge[0] - 1);
+        }
+        return new int[] {};
+    }
+
+    private boolean isConnected(int src, int target, boolean[] visited, List<Integer>[] adjList) {
+        visited[src] = true;
+        if (src == target) {
+            return true;
+        }
+
+        boolean isFound = false;
+        for (int adj : adjList[src]) {
+            if (!visited[adj]) {
+                isFound = isFound || isConnected(adj, target, visited, adjList);
+            }
+        }
+        return isFound;
+    }
+}
